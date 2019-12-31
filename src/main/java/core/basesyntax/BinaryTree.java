5b118b687cb3e868a766e74a1d9739525a103d16
Bohdan Chupika <boroda4436@users.noreply.github.com>
2019-12-31 13:46:08 +0200

    added task base implementation (#1)
    * added task base implementation
    
    * added test cases
    
    * added breadth first test case

---
+++ src/main/java/core/basesyntax/BinaryTree.java
@@ -0,0 +1,47 @@
+package core.basesyntax;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTree {
+    // Root of Binary Tree
+    private Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    /* Given a binary tree, print its nodes according to the 
+      "bottom-up" post order traversal. */
+    public List<Node> getNodesPostOrder(Node node) {
+        System.out.println("Please provide your implementation");
+
+        return new ArrayList<>();
+    }
+
+    /* Given a binary tree, print its nodes in inorder*/
+    public List<Node> getNodesInorder(Node node) {
+        System.out.println("Please provide your implementation");
+        return new ArrayList<>();
+    }
+
+    /* Given a binary tree, print its nodes in pre order*/
+    public List<Node> getNodesPreOrder(Node node) {
+        System.out.println("Please provide your implementation");
+        return new ArrayList<>();
+    }
+
+    /* Given a binary tree, print its nodes in pre order*/
+    public List<Node> getNodesBreadthFirst(Node node) {
+        System.out.println("Please provide your implementation");
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/core/basesyntax/BinaryTreeTest.java
@@ -0,0 +1,87 @@
+package core.basesyntax;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinaryTreeTest {
+    private BinaryTree binaryTree;
+
+    @Before
+    public void setUp() {
+        binaryTree = new BinaryTree();
+        binaryTree.setRoot(new Node(1));
+        binaryTree.getRoot().setLeft(new Node(2));
+        binaryTree.getRoot().setRight(new Node(3));
+        binaryTree.getRoot().getLeft().setLeft(new Node(4));
+        binaryTree.getRoot().getLeft().setRight(new Node(5));
+    }
+
+    @Test
+    public void getNodesPostOrder() {
+        List<Integer> expected = new LinkedList<>();
+        expected.add(4);
+        expected.add(5);
+        expected.add(2);
+        expected.add(3);
+        expected.add(1);
+
+        List<Integer> actual = binaryTree.getNodesPostOrder(binaryTree.getRoot())
+                .stream()
+                .map(Node::getKey)
+                .collect(Collectors.toList());
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNodesInOrder() {
+        List<Integer> expected = new LinkedList<>();
+        expected.add(4);
+        expected.add(2);
+        expected.add(5);
+        expected.add(1);
+        expected.add(3);
+
+        List<Integer> actual = binaryTree.getNodesInorder(binaryTree.getRoot())
+                .stream()
+                .map(Node::getKey)
+                .collect(Collectors.toList());
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNodesPreOrder() {
+        List<Integer> expected = new LinkedList<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(4);
+        expected.add(5);
+        expected.add(3);
+
+        List<Integer> actual = binaryTree.getNodesInorder(binaryTree.getRoot())
+                .stream()
+                .map(Node::getKey)
+                .collect(Collectors.toList());
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBreadthFirst() {
+        List<Integer> expected = new LinkedList<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
+        expected.add(5);
+
+        List<Integer> actual = binaryTree.getNodesInorder(binaryTree.getRoot())
+                .stream()
+                .map(Node::getKey)
+                .collect(Collectors.toList());
+        Assert.assertEquals(expected, actual);
+    }
+}
