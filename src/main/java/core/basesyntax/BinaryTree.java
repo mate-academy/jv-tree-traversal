f8acdc3d9b0f39a6b6ca70bb0d70524d35e82e09
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-01-01 14:57:06 +0200

    Added solution

--- src/main/java/core/basesyntax/BinaryTree.java
+++ src/main/java/core/basesyntax/BinaryTree.java
@@ -1,7 +1,9 @@
 package core.basesyntax;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTree {
     // Root of Binary Tree
@@ -22,26 +24,67 @@ public class BinaryTree {
     /* Given a binary tree, print its nodes according to the 
       "bottom-up" post order traversal. */
     public List<Node> getNodesPostOrder(Node node) {
-        System.out.println("Please provide your implementation");
-
-        return new ArrayList<>();
+        List<Node> postOrder = new ArrayList<>();
+        getPostOrder(postOrder, node);
+        return postOrder;
     }
 
     /* Given a binary tree, print its nodes in inorder*/
     public List<Node> getNodesInorder(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        List<Node> inOrder = new ArrayList<>();
+        getInOrder(inOrder, node);
+        return inOrder;
     }
 
     /* Given a binary tree, print its nodes in pre order*/
     public List<Node> getNodesPreOrder(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        List<Node> preOrder = new ArrayList<>();
+        getPreOrder(preOrder, node);
+        return preOrder;
     }
 
     /* Given a binary tree, print its nodes in pre order*/
     public List<Node> getNodesBreadthFirst(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        List<Node> breadthFirst = new ArrayList<>();
+        queue.add(node);
+        while (queue.peek() != null) {
+            Node temp = queue.poll();
+            breadthFirst.add(temp);
+            if (temp.getLeft() != null) {
+                queue.add(temp.getLeft());
+            }
+            if (temp.getRight() != null) {
+                queue.add(temp.getRight());
+            }
+        }
+        return breadthFirst;
+    }
+
+    private void getPostOrder(List<Node> postOrder, Node node) {
+        if (node == null) {
+            return;
+        }
+        getPostOrder(postOrder, node.getLeft());
+        getPostOrder(postOrder, node.getRight());
+        postOrder.add(node);
+    }
+
+    private void getInOrder(List<Node> inOrder, Node node) {
+        if (node == null) {
+            return;
+        }
+        getInOrder(inOrder, node.getLeft());
+        inOrder.add(node);
+        getInOrder(inOrder, node.getRight());
+    }
+
+    private void getPreOrder(List<Node> preOrder, Node node) {
+        if (node == null) {
+            return;
+        }
+        preOrder.add(node);
+        getPreOrder(preOrder, node.getLeft());
+        getPreOrder(preOrder, node.getRight());
     }
 }
