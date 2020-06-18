5dc8b37ad03508d4ff2d04e0012884c7d618a6e2
Максим Матуляк <maksymmatuliak@gmail.com>
2020-06-18 15:57:29 +0300

    renamed variables and fixed style

--- src/main/java/core/basesyntax/BinaryTree.java
+++ src/main/java/core/basesyntax/BinaryTree.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BinaryTree {
-    // Root of Binary Tree
+
     private Node root;
 
     public BinaryTree() {
@@ -19,69 +19,70 @@ public class BinaryTree {
         this.root = root;
     }
 
-    /* Given a binary tree, print its nodes according to the 
-      "bottom-up" post order traversal. */
     public List<Node> getNodesPostOrder(Node node) {
         List<Node> postOrder = new ArrayList<>();
         postOrder(node, postOrder);
         return postOrder;
     }
 
-    private void postOrder(Node node, List<Node> postOrder){
-        if (node == null) return;
+    private void postOrder(Node node, List<Node> postOrder) {
+        if (node == null) {
+            return;
+        }
         postOrder(node.getLeft(), postOrder);
         postOrder(node.getRight(), postOrder);
         postOrder.add(node);
     }
 
-    /* Given a binary tree, print its nodes in inorder*/
     public List<Node> getNodesInorder(Node node) {
         List<Node> inOrder = new ArrayList<>();
         inOrder(node, inOrder);
         return inOrder;
     }
 
-    private void inOrder(Node node, List<Node> inOrder){
-        if (node == null) return;
-        inOrder(node.getLeft(), inOrder);
-        inOrder.add(node);
-        inOrder(node.getRight(), inOrder);
-    }
-
-    /* Given a binary tree, print its nodes in pre order*/
     public List<Node> getNodesPreOrder(Node node) {
         List<Node> preOrder = new ArrayList<>();
         preOrder(node, preOrder);
         return preOrder;
     }
 
-    private void preOrder(Node node, List<Node> preOrder){
-        if (node == null) return;
+    public List<Node> getNodesBreadthFirst(Node node) {
+        return getListForNodesBreadFirst(node);
+    }
+
+    private void inOrder(Node node, List<Node> inOrder) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.getLeft(), inOrder);
+        inOrder.add(node);
+        inOrder(node.getRight(), inOrder);
+    }
+
+    private void preOrder(Node node, List<Node> preOrder) {
+        if (node == null) {
+            return;
+        }
         preOrder.add(node);
         preOrder(node.getLeft(), preOrder);
         preOrder(node.getRight(), preOrder);
     }
 
-    /* Given a binary tree, print its nodes in pre order*/
-    public List<Node> getNodesBreadthFirst(Node node) {
-        return bfs(node);
-    }
-
-    private List<Node> bfs(Node node) {
+    private List<Node> getListForNodesBreadFirst(Node node) {
         List<Node> queue = new ArrayList<>();
-        List<Node> list = new ArrayList<>();
+        List<Node> result = new ArrayList<>();
         queue.add(node);
-        while(queue.size() > 0){
+        while (queue.size() > 0) {
             Node tempNode = queue.get(0);
             queue.remove(0);
-            list.add(tempNode);
-            if (tempNode.getLeft() != null){
+            result.add(tempNode);
+            if (tempNode.getLeft() != null) {
                 queue.add(tempNode.getLeft());
             }
-            if (tempNode.getRight() != null){
+            if (tempNode.getRight() != null) {
                 queue.add(tempNode.getRight());
             }
         }
-        return list;
+        return result;
     }
 }
