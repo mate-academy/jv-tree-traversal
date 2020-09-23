ab1a79f6111ed82bf0b38e08b1113ed73d5a2f65
dariiapikul <dariyapikul@gmail.com>
2020-09-23 16:19:44 +0300

    Get list of Nodes in the certain order (binary tree) methods implemented

--- src/main/java/core/basesyntax/BinaryTree.java
+++ src/main/java/core/basesyntax/BinaryTree.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BinaryTree {
     // Root of Binary Tree
@@ -22,26 +24,51 @@ public class BinaryTree {
     /* Given a binary tree, print its nodes according to the 
       "bottom-up" post order traversal. */
     public List<Node> getNodesPostOrder(Node node) {
-        System.out.println("Please provide your implementation");
-
-        return new ArrayList<>();
+        List<Node> result = new ArrayList<>();
+        if (node.getLeft() != null) {
+            result.addAll(getNodesPostOrder(node.getLeft()));
+        }
+        if (node.getRight() != null) {
+            result.addAll(getNodesPostOrder(node.getRight()));
+        }
+        result.add(node);
+        return result;
     }
 
     /* Given a binary tree, print its nodes in inorder*/
     public List<Node> getNodesInorder(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        List<Node> result = new ArrayList<>();
+        if (node.getLeft() != null) {
+            result.addAll(getNodesInorder(node.getLeft()));
+        }
+        result.add(node);
+        if (node.getRight() != null) {
+            result.addAll(getNodesInorder(node.getRight()));
+        }
+        return result;
     }
 
     /* Given a binary tree, print its nodes in pre order*/
     public List<Node> getNodesPreOrder(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        List<Node> result = new ArrayList<>();
+        result.add(node);
+        if (node.getLeft() != null) {
+            result.addAll(getNodesPreOrder(node.getLeft()));
+        }
+        if (node.getRight() != null) {
+            result.addAll(getNodesPreOrder(node.getRight()));
+        }
+        return result;
     }
 
-    /* Given a binary tree, print its nodes in pre order*/
+    /* Given a binary tree, print its nodes in breadth order*/
     public List<Node> getNodesBreadthFirst(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        return getNodesPreOrder(node).stream()
+                .collect(Collectors.groupingBy(Node::getKey))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .map(value -> value.get(0))
+                .collect(Collectors.toList());
     }
 }
