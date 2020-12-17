475f9c5784af3917ae50d535645f76de45ece969
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-17 13:28:55 +0200

    alghoritms DFT and BFT was implemented

--- src/main/java/core/basesyntax/BinaryTree.java
+++ src/main/java/core/basesyntax/BinaryTree.java
@@ -22,26 +22,61 @@ public class BinaryTree {
     /* Given a binary tree, print its nodes according to the 
       "bottom-up" post order traversal. */
     public List<Node> getNodesPostOrder(Node node) {
-        System.out.println("Please provide your implementation");
-
-        return new ArrayList<>();
+        if (node == null) {
+            return new ArrayList<>();
+        }
+        List<Node> list = new ArrayList<>();
+        list.addAll(getNodesPostOrder(node.getLeft()));
+        list.addAll(getNodesPostOrder(node.getRight()));
+        list.add(node);
+        return list;
     }
 
     /* Given a binary tree, print its nodes in inorder*/
     public List<Node> getNodesInorder(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        if (node == null) {
+            return new ArrayList<>();
+        }
+        List<Node> list = new ArrayList<>();
+        list.addAll(getNodesInorder(node.getLeft()));
+        list.add(node);
+        list.addAll(getNodesInorder(node.getRight()));
+        return list;
     }
 
     /* Given a binary tree, print its nodes in pre order*/
     public List<Node> getNodesPreOrder(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        if (node == null) {
+            return new ArrayList<>();
+        }
+        List<Node> list = new ArrayList<>();
+        list.add(node);
+        list.addAll(getNodesPreOrder(node.getLeft()));
+        list.addAll(getNodesPreOrder(node.getRight()));
+        return list;
     }
 
     /* Given a binary tree, print its nodes in pre order*/
     public List<Node> getNodesBreadthFirst(Node node) {
-        System.out.println("Please provide your implementation");
-        return new ArrayList<>();
+        if (node == null) {
+            return new ArrayList<>();
+        }
+        List<Node> list = new ArrayList<>();
+        if (node == root) {
+            list.add(node);
+        }
+        if (node.getLeft() != null) {
+            list.add(node.getLeft());
+        }
+        if (node.getRight() != null) {
+            list.add(node.getRight());
+        }
+        if (node.getLeft() != null) {
+            list.addAll(getNodesBreadthFirst(node.getLeft()));
+        }
+        if (node.getRight() != null) {
+            list.addAll(getNodesBreadthFirst(node.getRight()));
+        }
+        return list;
     }
 }
